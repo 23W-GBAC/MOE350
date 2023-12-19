6c07efdf8700517f1520ec88fc396b627d5e446a
mohammadmasry <moe.al.my@gmail.com>
2023-12-19 11:56:01 +0100

    small edit

--- resizing_images.sh
+++ resizing_images.sh
@@ -1,22 +1,25 @@
-if ! -d "img"; then 
-        mkdir img
-else 
-        echo "The directory is already exists. Skipping write directory."
-fi 
+#!/bin/bash
 
-if ! -f "resized_images_list.md"; then 
-        touch resized_images_list.md
+# Input directory containing your BMW images
+input_dir="Images/BMW_images"
+
+# Output directory for resized images
+output_dir="resized_images"
+
+# Desired size for the images
+desired_size="600x600"  # Change this to your preferred size (e.g., 600x600)
+
+# Check if the output directory exists; if not, create it
+if [ ! -d "$output_dir" ]; then
+    echo "Creating output directory: $output_dir"
+    mkdir -p "$output_dir"
 else
-        echo "The file is already exists. Skipping file creation."
-fi 
+    echo "Output directory '$output_dir' already exists."
+fi
 
-for file in *.jpg *.jpeg *.png *.gif *.JPG *.JPEG; do
-        filename=$(basename "$file")
-
-        if ! grep -q "$filename" "resized_images_list.md"; then
-                convert "$file" -resize "600x600>" "img/$filename"
-
-                echo "$filename" >> resized_images_list.md
-        fi
+# Loop through each image in the input directory and resize
+for file in "$input_dir"/*.{jpg,jpeg,png,gif,JPG,JPEG}; do
+    filename=$(basename "$file")
+    convert "$file" -resize "$desired_size>" "$output_dir/$filename"
+    echo "Resized $filename"
 done
-
